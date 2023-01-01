869a65ccd1e125e74b564df9f2348ff0ca2d2566
ChristopheCC <christophe.chane@gmail.com>
2023-01-01 15:38:30 +0800

    Initial commit

---
+++ src/main/java/com/udemy/estore/core/events/ProductReservedEvent.java
@@ -0,0 +1,15 @@
+package com.udemy.estore.core.events;
+
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+public class ProductReservedEvent {
+
+    private final String productId;
+    private final int quantity;
+    private final String orderId;
+    private final String userId;
+}
